6241f0ad55e96e43c9257809e63a18e8b5df7bcf
Sidharth Bhasin <sid21bhasin@gmail.com>
2023-10-13 22:53:33 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./index.css";
+const App = () => {
+  return (
+    <main className="relative">
+      {" "}
+      nav
+      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="padding"> Popular Products</section>
+      <section className="padding"> Super Quality</section>
+      <section className="padding"> Services</section>
+      <section className="padding"> special Offer</section>
+      <section className="padding bg-pale-blue"> Customer Reviews</section>
+      <section className="padding-x sm:py-32 py-16 w-full"> Subscribes</section>
+      <section className="padding bg-black padding-x padding-t pb-8">
+        {" "}
+        Footer
+      </section>
+    </main>
+  );
+};
+
+export default App;
