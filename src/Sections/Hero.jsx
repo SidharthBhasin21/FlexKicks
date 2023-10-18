d73d531f7d988d2420cd09c98752d1b60a07f702
Sidharth Bhasin <sid21bhasin@gmail.com>
2023-10-18 23:59:29 +0530

    Added: hero section added

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import Button from "../Components/Button";
+import { arrowRight } from "../assets/icons";
+import { statistics } from "../Constants";
+import { bigShoe1 } from "../assets/images";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col gap-10 max-container justify-center min-h-screen"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer Collection
+        </p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold   ">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            {" "}
+            The New Arrival
+          </span>
+          <br />
+          <span className="text-coral-red inline-block mt-3">FlexKick</span>
+          Shoes
+        </h1>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mb-14 sm:max-w-sm">
+          Discover stylish FlexKick arrivals, Quality comfort, and innovation
+          for your active life.
+        </p>
+        <Button label="Shop Now" iconUrl={arrowRight} />
+
+        <div className="flex justify-center items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat, index) => (
+            <div key={stat.label}>
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+              <p className="font-montserrat leading-7 text-slate-gray">
+                {stat.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="relative flex-1 flex  justify-center  items-center xl:min-h-screen max-xl:py-40 bg-primary big-hero bg-cover bg-center">
+        <img
+          src={bigShoe1}
+          alt="shoe banner "
+          width={610}
+          height={500}
+          className="object-contain relative z-10"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
