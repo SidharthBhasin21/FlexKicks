63741d6c5d041b200944efabbf9bc18855a37978
Sidharth Bhasin <sid21bhasin@gmail.com>
2023-10-25 21:58:48 +0530

    Added: Super Quality section

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -5,11 +5,13 @@ const Button = ({ label, iconUrl }) => {
     <button className="flex justify-center items-center px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full">
       {" "}
       {label}{" "}
-      <img
-        src={iconUrl}
-        alt="ArrowButtonIcon"
-        className="ml-2 rounded-full w-5 h-5"
-      />
+      {iconUrl && (
+        <img
+          src={iconUrl}
+          alt="ArrowButtonIcon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
   );
 };
