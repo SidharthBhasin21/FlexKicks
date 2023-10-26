8a376b8ece15316fd50127de6c8f80638c9591ba
Sidharth Bhasin <sid21bhasin@gmail.com>
2023-10-26 20:53:19 +0530

    Added: Sercives section

--- src/Sections/Services.jsx
+++ src/Sections/Services.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { services } from "../Constants";
+import ServiceCard from "../Components/ServiceCard";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="flex max-container justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard
+          key={service.label}
+          label={service.label}
+          imageURL={service.imgURL}
+          subtext={service.subtext}
+        />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
